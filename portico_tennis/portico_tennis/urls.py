893b1baad5794aacd7acef5e892467460215cad7
Amit Shokeen <ashokeen@Amits-MacBook-Pro.local>
2025-01-20 14:38:13 +1100

    changes to style, login, and reset pages

--- portico_tennis/portico_tennis/urls.py
+++ portico_tennis/portico_tennis/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path, include
 
 admin.site.site_header = "Portico Tennis Administration"
@@ -24,4 +25,8 @@ admin.site.index_title = "Portico Tennis Admin"
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('users.urls')),
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
