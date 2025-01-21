0d7d9c98790df04060888e509458eddee1c8b1ae
Amit Shokeen <ashokeen@Amits-MacBook-Pro.local>
2025-01-21 16:30:42 +1100

    invitation codes

--- portico_tennis/users/forms.py
+++ portico_tennis/users/forms.py
@@ -3,7 +3,29 @@ from .models import CustomUser
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth.forms import PasswordResetForm
 
+# Invitation codes are hard-coded here. Not the best practice. 
+# To be improved: 1. store codes in a db and validate against them dynamically.
+# 2. Use of regex patterns to match invitation code formats
+# 3. Use e-mail to send one time codes and verify them... this can be interesting...
+INVITATION_CODES = ["TENNIS2025", "tennis2025", "PLAY2025", "play2025", "testnow"]
+
 class CustomUserCreationForm(UserCreationForm):
+    invitation_code = forms.CharField(
+        required=True,
+        widget=forms.TextInput(attrs={
+            'class': 'form-control', 
+            'placeholder': 'Enter invitation code', 
+            'id': 'id_invitation_code',
+            'autofocus': True
+        }),
+        label="Invitation Code"
+    )
+    def clean_invitation_code(self):
+        code = self.cleaned_data.get('invitation_code')
+        if code not in INVITATION_CODES:
+            raise forms.ValidationError("Invalid invitation code.")
+        return code
+    
     email = forms.EmailField(
         widget=forms.EmailInput(attrs={
             'class': 'form-control',
@@ -21,7 +43,7 @@ class CustomUserCreationForm(UserCreationForm):
 
     class Meta:
         model = CustomUser
-        fields = ['username', 'email', 'phone_number', 'password1', 'password2']
+        fields = ['username', 'email', 'phone_number', 'password1', 'password2', 'invitation_code']
         widgets = {
             'username': forms.TextInput(attrs={
                 'class': 'form-control',
